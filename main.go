2b428088e9430a054c0410928d1f726fa5fc05ea
wxcgeek <wxchello@qq.com>
2020-05-09 19:37:18 +0800

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 	"github.com/coyove/iis/common"
 	"github.com/coyove/iis/dal"
 	"github.com/coyove/iis/dal/forgettable/goforget"
-	"github.com/coyove/iis/dal/kv/cache"
+	"github.com/wxcgeek/iis2/dal/kv/cache"
 	"github.com/coyove/iis/ik"
 	"github.com/coyove/iis/middleware"
 	"github.com/coyove/iis/model"
